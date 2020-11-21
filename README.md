d8dd95e44095ddd97e88e746a78c0ed1b1069151
Helmiina Kontio <helmiina.kontio@gmail.com>
2020-11-21 18:37:13 +0200

    uibuild

---
+++ README.md
@@ -0,0 +1,1 @@
+[Sovellus on julkaistu osoitteeseen](https://sleepy-beyond-28705.herokuapp.com/)
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 
 app.use(express.json())
+app.use(express.static('build'))
 
 // morgan
 const morgan = require('morgan')
